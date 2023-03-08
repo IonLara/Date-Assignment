eff56a0ef60f4e5dd2d191a4f9286a5d5a02e8a0
SebasMac <ionrodriguezlara@gmail.com>
2023-03-07 17:25:43 -0800

    Added initializers to Date struct

--- Date/Date/Date.swift
+++ Date/Date/Date.swift
@@ -8,7 +8,51 @@
 import Foundation
 
 struct Date {
-  
+    private (set) var day: Int
+    private (set) var month: Int
+    private (set) var year: Int
+    
+    
+    init(month: Int, day: Int, year: Int) {
+        if month > 12 || month < 1 {
+            self.day = 1
+            self.month = 1
+            self.year = 2000
+            return
+        } else {
+            self.month = month
+        }
+        if year >= 0 {
+            self.year = year
+        } else {
+            self.day = 1
+            self.month = 1
+            self.year = 2000
+        }
+        var limit = -1
+        switch month {
+        case 2:
+            let leap = year % 4 == 0 ? 1 : 0
+            limit = 28 + leap
+        case 1, 3, 5, 7, 8, 10, 12:
+            limit = 31
+        default:
+            limit = 30
+        }
+        if day > limit || day < 1 {
+            self.day = 1
+            self.month = 1
+            self.year = 2000
+            return
+        } else {
+            self.day = day
+        }
+    }
+    init() {
+        self.day = 1
+        self.month = 1
+        self.year = 2000
+    }
 }
 
 enum DateFormat {
